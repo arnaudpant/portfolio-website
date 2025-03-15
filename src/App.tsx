0c37802bb44652647448e388f880ce85492f0a3e
macbook kal <arnaud.pantenier@gmail.com>
2025-03-15 21:12:06 +0100

    FIRST COMMIT

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import "./App.css";
+import Hero from "./components/Hero";
+
+function App() {
+
+    return (
+
+            <div className="min-h-screen bg-gray-800">
+                <Hero />
+            </div>
+
+    );
+}
+
+export default App;
---
+++ src/components/Hero.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+
+
+const Hero = () => {
+    return (
+        <div className="w-full h-screen">
+            <Navbar />
+        </div>
+    );
+};
+
+export default Hero;
+
+// 20:29
