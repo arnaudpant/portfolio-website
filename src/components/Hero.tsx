a11e7be7fd21623183646968c9722dcaed15df44
macbook kal <arnaud.pantenier@gmail.com>
2025-04-05 18:47:44 +0200

    Hero Hello

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,26 @@
 import Navbar from "./Navbar";
+import { letters } from "../data";
 
 
 const Hero = () => {
     return (
-        <div className="w-full h-screen">
+        <div className="w-full h-screen flex flex-col justify-center items-center">
             <Navbar />
+            <div className="flex flex-col md:items-center items-start xl:gap-y-10 gap-y-3 xl:mb-0 md:mb-20 mb-0">
+                <h1 className="flex flex-col xl:space-y-8 md:space-y-4 space-y-2 xl:text-6xl md:text-4xl text-3xl md:font-normal font-bold text-yellow-500">
+                    <span className="flex">
+                        {
+                            letters.map((letter, index) => (
+                                <span key={index} className="inline-block md:w-38 w-32 xl:-mr-20 -mr-24">{letter.char}</span>
+                            ) )
+                        }
+                    </span>
+                </h1>
+            </div>
         </div>
-    );
+    ); 
 };
 
 export default Hero;
 
-// 27:52
+// 34:52
---
+++ src/data/index.ts
@@ -0,0 +1,16 @@
+type LettersProps = {
+    char: string, img: string, rotate: string
+}
+
+export const letters: LettersProps[] = [
+    {char: 'H', img: "", rotate: '-rotate-15'},
+    {char: 'e', img: "", rotate: '-rotate-15'},
+    {char: 'l', img: "", rotate: '-rotate-15'},
+    {char: 'l', img: "", rotate: '-rotate-15'},
+    {char: 'o', img: "", rotate: '-rotate-15'},
+]
+
+export const socialIcons: {icon: string} [] = [
+    {icon: 'bx bxl-instagram-alt'},
+    {icon: 'bx bxl-twitter-alt'}
+]
